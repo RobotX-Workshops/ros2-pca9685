c9807a299cbc213f0798244f6df72828c3495b38
Andrew Johnson <andrewmjohnson549@gmail.com>
2025-03-10 14:06:20 +0100

    Update package docs

--- setup.py
+++ setup.py
@@ -1,30 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'pca9685'
+package_name = "pca9685"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=[
-	'setuptools',
-	'Adafruit-PCA9685'
-	],
+    install_requires=["setuptools", "Adafruit-PCA9685"],
     zip_safe=True,
-    maintainer='aj',
-    maintainer_email='andrewmjohnson549@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="aj",
+    maintainer_email="andrewmjohnson549@gmail.com",
+    description="A package for sening pulse width modulation signals to a PCA9685 board",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'subscriber = pca9685.subscriber:main'
-        ],
+        "console_scripts": ["subscriber = pca9685.subscriber:main"],
     },
-
 )
