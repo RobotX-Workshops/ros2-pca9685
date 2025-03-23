3ba9ab71c307324d5bb6f0e03e321c7df77c1e03
Andrew Johnson <andrewmjohnson549@gmail.com>
2025-03-23 15:22:47 +0100

    Update Subscriber

--- pca9685/subscriber.py
+++ pca9685/subscriber.py
@@ -2,29 +2,34 @@ from pca9685.models.config import PCA9685Config
 from pca9685.services.pca9685 import PCA9685
 import rclpy  # Import ROS 2 Python client library
 from rclpy.node import Node  # Import the Node class
-from std_msgs.msg import Int32MultiArray  # Import standard message type for two integers (channel, pulse)
+from std_msgs.msg import (
+    Int32MultiArray,
+)  # Import standard message type for two integers (channel, pulse)
+
 
 class PCA9685SubscriberNode(Node):
     def __init__(self):
-        super().__init__('pca9685_subscriber')
-        
-        # Declare parameters with default values
-        self.declare_parameter('bus', 1)
-        self.declare_parameter('address', 0x40)
-        
-        # Retrieve parameter values
-        bus = self.get_parameter('bus').value
-        address = self.get_parameter('address').value
+        super().__init__("pca9685_node")
 
-        config = PCA9685Config(busnum=bus, address=address, frequency=60)
+        # Declare parameters with default values
+        self.declare_parameter("bus", 1)
+        self.declare_parameter("address", 0x40)
+        self.declare_parameter("frequency", 60)
+
+        # Retrieve parameter values
+        bus = self.get_parameter("bus").get_parameter_value().integer_value
+        address = self.get_parameter("address").get_parameter_value().integer_value
+        frequency = self.get_parameter("frequency").get_parameter_value().integer_value
+
+        config = PCA9685Config(busnum=bus, address=address, frequency=frequency)
         self.pca9685 = PCA9685(config=config)
-        
-        # Create subscriber to listen on the `/pwm_channel` topic
+
+        # Create subscriber to listen on the `/pwm_command` topic
         self.subscription = self.create_subscription(
             Int32MultiArray,
-            '/pwm_channel',
+            "/pwm_command",
             self.listener_callback,
-            10  # Queue size
+            10,  # Queue size
         )
 
     def listener_callback(self, msg: Int32MultiArray):
@@ -32,10 +37,9 @@ class PCA9685SubscriberNode(Node):
         if len(msg.data) == 2:
             channel = msg.data[0]
             pulse = msg.data[1]
-            self.get_logger().info(f'Setting channel {channel} to pulse {pulse}')
             self.pca9685.set_pulse(channel, pulse)
         else:
-            self.get_logger().warn('Received message with unexpected length')
+            self.get_logger().error("Received message with unexpected length")
 
 
 def main(args=None):
@@ -51,6 +55,5 @@ def main(args=None):
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
