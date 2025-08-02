4d548023812140162a709fee7853440168ab9b1b
Andrew Johnson <andrewmjohnson549@gmail.com>
2025-08-02 09:51:12 +0200

    Fix path

--- pca9685_cpp/src/main.cpp
+++ pca9685_cpp/src/main.cpp
@@ -1,15 +1,15 @@
 #include "rclcpp/rclcpp.hpp"
-#include "pca9685_cpp/pca9685_node.hpp"
+#include "pca9685/pca9685_node.hpp"
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor exec;
   rclcpp::NodeOptions options;
-  
-  auto node = std::make_shared<pca9685_cpp::PCA9685Node>(options);
+
+  auto node = std::make_shared<pca9685::PCA9685Node>(options);
   exec.add_node(node);
-  
+
   exec.spin();
   rclcpp::shutdown();
   return 0;
